537b42184b2d4691162c155905752a40c5da09f9
Nicolas Saporiti <nicolas.saporiti@gmail.com>
2020-10-30 18:37:20 -0300

    Word Counter

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -3,8 +3,10 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
+import wordCounter from "../../components/wordCounter";
 
 export default function Post({ postData }) {
+  const readingTime = wordCounter(postData.contentHtml);
   return (
     <Layout>
       <Head>
@@ -14,8 +16,9 @@ export default function Post({ postData }) {
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
         <div className={`${utilStyles.lightText} ${utilStyles.inline}`}>
           <p>Por {postData.author} /&nbsp;</p>
-          <p><Date dateString={postData.date} /></p>
+          <p><Date dateString={postData.date}/></p>
         </div>
+        <p className={utilStyles.lightText}>Lectura de {readingTime} min</p>
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
