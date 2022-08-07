fb910d1cab78127b2e7dc2f423c3c9dc3b71978c
Nicolas Saporiti <nicolas.saporiti@gamil.com>
2022-08-07 19:14:21 -0400

    change tip button

--- components/tipButton.js
+++ components/tipButton.js
@@ -1,13 +1,17 @@
-import React from "react";
-import styles from "./tipButton.module.css";
+import React from 'react';
+import styles from './tipButton.module.css';
 
 export const tipButton = () => {
   const url = process.env.NEXT_PUBLIC_BTC_URL;
 
   return (
     <div>
-      <a className={styles.btc_button} href={"lightning:nicolas.saporiti@rayito.app"} target="_blank">
-        Donar BTC con Rayito.app ⚡ 
+      <a
+        className={styles.btc_button}
+        href={'https://rayito.app/nicolas'}
+        target='_blank'
+      >
+        Donar BTC con Rayito.app⚡
       </a>
     </div>
   );
