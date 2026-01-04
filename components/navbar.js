84283870c81bd898519cbbcf897ca7fb450e5ebe
Nicolas Saporiti <nicolas.saporiti@gmail.com>
2026-01-04 18:28:37 -0300

    fix css styles on post and nav

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import styles from "./navbar.module.css";
 import Link from "next/link";
 
@@ -7,25 +6,23 @@ const navbar = () => {
   return (
     <div className={styles.navbar}>
       <Link href="/about">
-          <Image
+        <a>
+          <img
             src="/images/about.png"
             alt="about"
             className={styles.iconStyle}
-            width={24}
-            height={24}
           />
+        </a>
       </Link>
       <a
         href="https://github.com/nicosaporiti"
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Image
+        <img
           src="/images/github.png"
           alt="githubIcon"
           className={styles.iconStyle}
-          width={24}
-          height={24}
         />
       </a>
       <a
@@ -33,22 +30,14 @@ const navbar = () => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        <Image
+        <img
           src="/images/linkedin.png"
           alt="linkedIN"
           className={styles.iconStyle}
-          width={24}
-          height={24}
         />
       </a>
       <a href="mailto:nicolas@saporiti.cl?subject=Contacto%20desde%20Blog">
-        <Image
-          src="/images/email.png"
-          alt="email"
-          className={styles.iconStyle}
-          width={24}
-          height={24}
-        />
+        <img src="/images/email.png" alt="email" className={styles.iconStyle} />
       </a>
     </div>
   );
