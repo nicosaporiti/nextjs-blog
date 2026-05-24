fcd7742be228ab969fa99e6297bccb37bedbf824
Nicolas Saporiti <nicolas.saporiti@gmail.com>
2026-05-24 19:11:50 -0300

    feat: add custom GA page view tracking

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,27 @@ import '../styles/global.css';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from '../contexts/themeContext';
 import Script from 'next/script';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { GA_ID, sendPageView } from '../lib/gtag';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const GA_ID = 'G-PXKER2TTH1';
-
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!router.isReady) {
+      return undefined;
+    }
+
+    const timeoutId = window.setTimeout(() => {
+      sendPageView(router.asPath, pageProps);
+    }, 0);
+
+    return () => window.clearTimeout(timeoutId);
+  }, [router.asPath, router.isReady]);
+
   return (
     <ThemeProvider>
       <div className={inter.className}>
@@ -18,9 +33,9 @@ export default function App({ Component, pageProps }) {
         <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_ID}');
+            window.gtag = window.gtag || function gtag(){window.dataLayer.push(arguments);}
+            window.gtag('js', new Date());
+            window.gtag('config', '${GA_ID}', { send_page_view: false });
           `}
         </Script>
         <Component {...pageProps} />
